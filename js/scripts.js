ed827c00453e52f41eb0f215c0360b1cb4dff24d
Vyacheslav Svetlichny <vyacheslavsvetlichny@gmail.com>
2024-08-10 15:17:35 +0700

    cart

--- js/scripts.js
+++ js/scripts.js
@@ -153,4 +153,36 @@ $(function () {
       $.fancybox.close()
     })
   })
+
+  $(function () {
+    $('.active-orders__item').click(function () {
+      $(this).children('.active-orders__additional').slideToggle()
+      $(this).toggleClass('active')
+    })
+  })
+
+  $('.select').on('click', '.select__head', function () {
+    if ($(this).hasClass('open')) {
+      $(this).removeClass('open')
+      $(this).next().fadeOut()
+    } else {
+      $('.select__head').removeClass('open')
+      $('.select__list').fadeOut()
+      $(this).addClass('open')
+      $(this).next().fadeIn()
+    }
+  })
+
+  $('.select').on('click', '.select__item', function () {
+    $('.select__head').removeClass('open')
+    $(this).parent().fadeOut()
+    $(this).parent().prev().text($(this).text())
+  })
+
+  $(document).click(function (e) {
+    if (!$(e.target).closest('.select').length) {
+      $('.select__head').removeClass('open')
+      $('.select__list').fadeOut()
+    }
+  })
 })
